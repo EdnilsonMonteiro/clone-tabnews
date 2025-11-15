8d6d30c2181d83647df55aeaf2e84289aed03dc1
Ednilson <103152011+EdnilsonMonteiro@users.noreply.github.com>
2025-11-15 21:39:16 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>A maioria das decisões são más decisões, se sinta honrado de ter a oportunidade de fazer as melhores.</h1>
+  return (
+    <h1>
+      A maioria das decisões são más decisões, se sinta honrado de ter a
+      oportunidade de fazer as melhores.
+    </h1>
+  );
 }
 
 export default Home;
