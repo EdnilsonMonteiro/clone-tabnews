f28fa4c3927e7d5bbe46c234d831904e46f4ccff
Ednilson Monteiro <ednilsonm.profissional@gmail.com>
2025-11-20 10:30:22 -0300

    add `jest.setup.js` e and `package.json` config for jest `.env.development` read

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,6 @@
+const LOCALHOST_PORT = process.env.LOCALHOST_PORT;
+
 test("GET to /api/v1/status should retorn 200", async () => {
-  const response = await fetch("http://localhost:3001/api/v1/status");
+  const response = await fetch(`http://localhost:${LOCALHOST_PORT}/api/v1/status`);
   expect(response.status).toBe(200);
 });
